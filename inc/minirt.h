6663b2f8c84c5dd1cbfb492600942954fdf51d86
Dani <danisarp@gmail.com>
2020-07-06 14:34:41 +0200

    small changes

--- inc/minirt.h
+++ inc/minirt.h
@@ -19,6 +19,91 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+typedef struct s_res
+{
+  int x_res;
+  int y_res;
+} t_res;
+
+typedef struct s_amb
+{
+  float light;
+  unsigned char color;
+} t_amb;
+
+typedef struct s_cam
+{
+  float coord[3];
+  float vector[3];
+  int fov;
+  struct s_cam *next;
+} t_cam;
+
+typedef struct s_light
+{
+  float coord[3];
+  float light;
+  unsigned char color;
+  struct s_light *next;
+} t_light;
+
+typedef struct s_sp
+{
+  float coord[3];
+  float d;
+  unsigned char color;
+  struct s_sp *next;
+} t_sp;
+
+typedef struct s_pl
+{
+  float coord[3];
+  float vector[3];
+  unsigned char color;
+  struct s_pl *next;
+} t_pl;
+
+typedef struct s_sq
+{
+  float coord[3];
+  float vector[3];
+  float side;
+  unsigned char color;
+  struct s_sq *next;
+} t_sq;
+
+typedef struct s_cy
+{
+  float coord[3];
+  float vector[3];
+  float d;
+  float h;
+  unsigned char color;
+  struct s_cy *next;
+} t_cy;
+
+typedef struct s_tr
+{
+  float first_coord[3];
+  float second_coord[3];
+  float third_coord[3];
+  unsigned char color;
+  struct s_tr *next;
+}
+
+typedef struct s_objlst
+{
+  t_res *res;
+  t_amb *amb;
+  t_cam *cam;
+  t_light *light;
+  t_sp *sp;
+  t_pl *pl;
+  t_sq *sq;
+  t_cy *cy;
+  t_tr *tr;
+}
+
 bool check_syntax_scene(char *scene_file);
 
 #endif
--- srcs/check_syntax_scene.c
+++ srcs/check_syntax_scene.c
@@ -12,12 +12,41 @@
 
 #include "../inc/minirt.h"
 
-
-
-/*static bool check_elem_list(char *scene_line)
+static int greater_length(char *s1, char *s2)
 {
-	if ()
-	}*/
+  int s1_len;
+  int s2_len;
+
+  s1_len = ft_strlen(s1);
+  s2_len = ft_strlen(s2);
+  return (s1_len > s2_len ? s1_len : s2_len);
+}
+
+static bool check_elem_list(char *scene_line)
+{
+  char *id;
+
+  id = scene_line[0];
+  if (!ft_strncmp(id, "R", greater_length(id, "R")))
+      return (res_error_check(scene_line));
+  if (!ft_strncmp(id, "A", greater_length(id, "A")))
+    return (a_error_check(scene_line));
+  if (!ft_strncmp(id, "c", greater_length(id, "c")))
+    return (c_error_check(scene_line));
+  if (!ft_strncmp(id, "l", greater_length(id, "l")))
+    return (l_error_check(scene_line));
+  if (!ft_strncmp(id, "sp", greater_length(id, "sp")))
+    return (sp_error_check(scene_line));
+  if (!ft_strncmp(id, "pl", greater_length(id, "pl")))
+    return (pl_error_check(scene_line));
+  if (!ft_strncmp(id, "sq", greater_length(id, "sq")))
+    return (sq_error_check(scene_line));
+  if (!ft_strncmp(id, "cy", greater_length(id, "cy")))
+    return (cy_error_check(scene_line));
+  if (!ft_strncmp(id, "tr", greater_length(id, "tr")))
+    return (tr_error_check(scene_line));
+  return (false);
+}
 
 bool check_syntax_scene(char *scene_file)
 {
@@ -27,6 +56,7 @@ bool check_syntax_scene(char *scene_file)
 	int fd;
 	int i;
 	char *line;
+        char **scene;
 	char **scene_line;
 
 	i = 0;
@@ -37,14 +67,14 @@ bool check_syntax_scene(char *scene_file)
 		}
 	while (get_next_line(fd, &line) == 1)
 		{
-		 if (!(scene_line = ft_split(line, ' ')))
+                  /*if (!(scene_line = ft_split(line, ' ')))
 				return (false);
 		 while (scene_line[i])
 				{
 					printf("%s\n", scene_line[1]);
 					i++;
 					}
-			printf("====\n");
+                                        printf("====\n");*/
 		}
 	return (true);
 }
